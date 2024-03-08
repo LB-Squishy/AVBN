440b13e4fc4a70100d14d537a88067a9ec8d36c5
Laurent Le Boulbar <laurentleboulbar@gmail.com>
2024-03-08 16:16:23 +0100

    refactorisation

---
+++ blog/src/model.php
@@ -0,0 +1,28 @@
+<?php
+
+function getPosts()
+{
+    // We connect to the database.
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=blog_avbn;charset=utf8', 'root', '');
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+
+    // We retrieve the 5 last blog posts.
+    $statement = $database->query(
+        "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
+    );
+    $posts = [];
+    while (($row = $statement->fetch())) {
+        $post = [
+            'title' => $row['title'],
+            'french_creation_date' => $row['date_creation_fr'],
+            'content' => $row['content'],
+        ];
+
+        $posts[] = $post;
+    }
+
+    return $posts;
+}
--- blog_avbn.sql
+++ blog_avbn.sql
@@ -24,21 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `billets`
+-- Structure de la table `posts`
 --
 
-CREATE TABLE `billets` (
+CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
-  `titre` varchar(255) NOT NULL,
-  `contenu` text NOT NULL,
-  `date_creation` datetime NOT NULL
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `creation_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Déchargement des données de la table `billets`
+-- Déchargement des données de la table `posts`
 --
 
-INSERT INTO `billets` (`id`, `titre`, `contenu`, `date_creation`) VALUES
+INSERT INTO `posts` (`id`, `title`, `content`, `creation_date`) VALUES
 (1, 'Bienvenue sur le blog de l\'AVBN !', 'Je vous souhaite à toutes et à tous la bienvenue sur le blog qui parlera de... l\'Association de VolleyBall de Nuelly !', '2022-02-17 16:28:41'),
 (2, 'L\'AVBN à la conquête du monde !', 'C\'est officiel, le club a annoncé à la radio hier soir \"J\'ai l\'intention de conquérir le monde !\".\r\nIl a en outre précisé que le monde serait à sa botte en moins de temps qu\'il n\'en fallait pour dire \"Association de VolleyBall de Nuelly\". Pas dur, ceci dit entre nous...', '2022-02-17 16:28:42');
 
@@ -47,9 +47,9 @@ INSERT INTO `billets` (`id`, `titre`, `contenu`, `date_creation`) VALUES
 --
 
 --
--- Index pour la table `billets`
+-- Index pour la table `posts`
 --
-ALTER TABLE `billets`
+ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -57,9 +57,9 @@ ALTER TABLE `billets`
 --
 
 --
--- AUTO_INCREMENT pour la table `billets`
+-- AUTO_INCREMENT pour la table `posts`
 --
-ALTER TABLE `billets`
+ALTER TABLE `posts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
