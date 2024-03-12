4139ba90691d6ee6dfb41e1470f8466784435375
Laurent Le Boulbar <laurentleboulbar@gmail.com>
2024-03-12 16:56:04 +0100

    Généralisation Namespace

--- blog/src/controllers/add_comment.php
+++ blog/src/controllers/add_comment.php
@@ -1,27 +1,33 @@
 <?php
 
-use Application\Model\Comment\CommentRepository;
+namespace Application\Controllers\AddComment;
 
+require_once('src/lib/database.php');
 require_once('src/model/comment.php');
 
-function addComment(string $post, array $input)
+use Application\Lib\Database\DatabaseConnection;
+use Application\Model\Comment\CommentRepository;
+
+class AddComment
 {
-    $author = null;
-    $comment = null;
+    public function execute(string $post, array $input)
+    {
+        $author = null;
+        $comment = null;
+        if (!empty($input['author']) && !empty($input['comment'])) {
+            $author = $input['author'];
+            $comment = $input['comment'];
+        } else {
+            throw new \Exception('Les données du formulaire sont invalides.');
+        }
 
-    if (!empty($input['author']) && !empty($input['comment'])) {
-        $author = $input['author'];
-        $comment = $input['comment'];
-    } else {
-        throw new Exception('Les données du formulaire sont invalides.');
-    }
-
-    $commentRepository = new CommentRepository();
-    $commentRepository->connection = new DatabaseConnection();
-    $success = $commentRepository->createComment($post, $author, $comment);
-    if (!$success) {
-        throw new Exception('Impossible d\'ajouter le commentaire !');
-    } else {
-        header('Location: index.php?action=post&id=' . $post);
+        $commentRepository = new CommentRepository();
+        $commentRepository->connection = new DatabaseConnection();
+        $success = $commentRepository->createComment($post, $author, $comment);
+        if (!$success) {
+            throw new \Exception('Impossible d\'ajouter le commentaire !');
+        } else {
+            header('Location: index.php?action=post&id=' . $post);
+        }
     }
 }
