46de307b45500769c7fe6e08e1d3f6a2c166a10f
Laurent Le Boulbar <laurentleboulbar@gmail.com>
2024-03-12 17:38:43 +0100

    Update Comment + readme

---
+++ README.md
@@ -0,0 +1,17 @@
+[![forthebadge](https://forthebadge.com/images/badges/powered-by-coffee.svg)](https://forthebadge.com)
+
+# Apprentissage de l'Architecture MVC en PHP
+
+## Technologie:
+
+-   PHP
+-   MySQL
+
+## Contenu:
+
+Découverte de l'Architecture Modèle-Vue-Controleur au travers un tutoriel de réalisation de blog qui aborde:
+
+-   Le MVC
+-   La POO
+-   La gestion des exceptions
+-   Les Namespaces
--- blog/src/model/comment.php
+++ blog/src/model/comment.php
@@ -8,28 +8,32 @@ use Application\Lib\Database\DatabaseConnection;
 
 class Comment
 {
+    public string $identifier;
     public string $author;
     public string $frenchCreationDate;
     public string $comment;
+    public string $post;
 }
 
 class CommentRepository
 {
     public DatabaseConnection $connection;
 
-    function getComments(string $post): array
+    public function getComments(string $post): array
     {
         $statement = $this->connection->getConnection()->prepare(
-            "SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM comments WHERE post_id = ? ORDER BY comment_date DESC"
+            "SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date, post_id FROM comments WHERE post_id = ? ORDER BY comment_date DESC"
         );
         $statement->execute([$post]);
 
         $comments = [];
         while (($row = $statement->fetch())) {
             $comment = new Comment();
+            $comment->identifier = $row['id'];
             $comment->author = $row['author'];
             $comment->frenchCreationDate = $row['french_creation_date'];
             $comment->comment = $row['comment'];
+            $comment->post = $row['post_id'];
 
             $comments[] = $comment;
         }
@@ -37,12 +41,44 @@ class CommentRepository
         return $comments;
     }
 
-    function createComment(string $post, string $author, string $comment)
+    public function getComment(string $identifier): ?Comment
+    {
+        $statement = $this->connection->getConnection()->prepare(
+            "SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date, post_id FROM comments WHERE id = ?"
+        );
+        $statement->execute([$identifier]);
+
+        $row = $statement->fetch();
+        if ($row === false) {
+            return null;
+        }
+
+        $comment = new Comment();
+        $comment->identifier = $row['id'];
+        $comment->author = $row['author'];
+        $comment->frenchCreationDate = $row['french_creation_date'];
+        $comment->comment = $row['comment'];
+        $comment->post = $row['post_id'];
+
+        return $comment;
+    }
+
+    public function createComment(string $post, string $author, string $comment): bool
     {
         $statement = $this->connection->getConnection()->prepare(
             'INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())'
         );
         $affectedLines = $statement->execute([$post, $author, $comment]);
+
+        return ($affectedLines > 0);
+    }
+
+    public function updateComment(string $identifier, string $author, string $comment): bool
+    {
+        $statement = $this->connection->getConnection()->prepare(
+            'UPDATE comments SET author = ?, comment = ? WHERE id = ?'
+        );
+        $affectedLines = $statement->execute([$author, $comment, $identifier]);
 
         return ($affectedLines > 0);
     }
